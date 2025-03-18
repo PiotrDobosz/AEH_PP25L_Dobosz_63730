c04c276dfca8071732bcc660c26d6b10c0476f56
Piotrek <dobosp2616595_aehit@students.vizja.pl>
2025-03-18 10:51:50 +0100

    Drugi zapis do Repozytorium

--- src/pl/pp/mojaDrugaAplikacja.java
+++ src/pl/pp/mojaDrugaAplikacja.java
@@ -2,6 +2,16 @@ package pl.pp;
 import java.util.Scanner;
 public class mojaDrugaAplikacja {
     public static void main(String[] args) {
+        // Zadanie 2.1
+        int x = 10;
+        int doubleX = x * 2;
+        int squareX = x * x;
+
+        System.out.println("x = " + x);
+        System.out.println("Dwukrotność x = " + doubleX);
+        System.out.println("x^2 = " + squareX);
+
+        // Zadanie 2.2
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj swój wiek w latach:");
         int age = scanner.nextInt();
@@ -11,4 +21,4 @@ public class mojaDrugaAplikacja {
 
         scanner.close();
     }
-    }
+}
